26dd65a282ada1e79309c4ff35cee4e49b086b66
Harrison <Harrison@sentdex.com>
2015-02-15 20:35:18 -0800

    Create part3.py

---
+++ part3.py
@@ -0,0 +1,56 @@
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load('racecar.png')
+
+def car(x,y):
+    gameDisplay.blit(carImg,(x,y))
+
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+x_change = 0
+
+crashed = False
+
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            if event.key == pygame.K_RIGHT:
+                x_change = 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+    
+            
+    x += x_change
+
+    gameDisplay.fill(white)
+    car(x,y)
+    
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
